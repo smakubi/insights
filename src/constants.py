8f03599df682f1f25f0f6f69d496bce712d96ec6
Samwel Emmanuel <samakubi@gmail.com>
2024-09-12 20:12:19 -0400

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,6 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-K1N4XBkXXJuMNX_V9ee0XM5uj9l-G4N
 LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME", "gpt-4-0125-preview")
 USER = os.getenv("USER", "root")
 PASSWORD = os.getenv("PASSWORD", "MamaGrentina$100!")
-HOST = os.getenv("HOST", "localhost")
+HOST = os.getenv("HOST", "127.0.0.1")
 DATABASE = os.getenv("DATABASE", "ecommerce")
 PORT = int(os.getenv("PORT", 3306))
